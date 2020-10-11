733bb72d4884a1db120a3a4b10c6c5b30393fe98
laowang <1249402434@qq.com>
2020-10-11 14:50:50 +0800

    finish

--- model/gaussian_blend_model.py
+++ model/gaussian_blend_model.py
@@ -26,30 +26,53 @@ class gaussian_EM:
         self.weights = [1 / K] * K
 
     def fit(self, X, epochs=100):
-        N = len(X)
-        dim = len(X[0])
-        mean_arr = np.random.randn(N, dim)
+        if type(X) == list:
+            X = np.array(X)
+        N, dim = X.shape
+        mean_arr = np.random.randn(self.K, dim)
         cov_matrix_arr = [np.diag(dim * [1]) for _ in range(self.K)]
 
         self.classifier_list = [gaussian_model(mean_arr[k], cov_matrix_arr[k]) for k in range(self.K)]
         res_arr = []
-
         for e in range(epochs):
             for i in range(N):
                 gamma_nk_arr = []
                 total = 0
                 for k in range(self.K):
-                    gamma_nk_arr.append(self.weights[k] * self.classifier_list[k].gaussian_prob(X[i]))
-                    total += self.weights[k] * self.classifier_list[k].gaussian_prob(X[i])
+                    val = self.weights[k] * self.classifier_list[k].gaussian_prob(X[i])
+                    gamma_nk_arr.append(val)
+                    total += val
 
                 gamma_nk_arr = [gamma_nk_arr[k] / total for k in range(self.K)]
                 res_arr.append(gamma_nk_arr)
 
             Nk_list = [sum(list(zip(*res_arr))[k]) for k in range(self.K)]
+            mean_new = []
+            cov_matrix_arr_new = []
             for k in range(self.K):
-                self.weights[i] = Nk_list[k] / N
-                mean_new = [res_arr]
-                cov_matrix_arr_new =
+                Nk = Nk_list[k]
+                self.weights[k] = Nk / N
+                mean_new.append(sum([X[i] * res_arr[i][k] for i in range(N)]) / Nk)
 
+            mean_new = np.array(mean_new)
+            for k in range(self.K):
+                Nk = Nk_list[k]
+                cov_matrix_arr_new.append(sum([(X[i]-mean_new[k]).dot((X[i]-mean_new[k]).T)*res_arr[i][k]
+                                               for i in range(N)])/Nk)
+
+            for k in range(self.K):
+                self.classifier_list[k].update(mean_new, cov_matrix_arr_new)
+
+    def predict(self, X):
+        if type(X)==list:
+            X = np.array(X)
+        length = X.shape[0]
+        y_pred = [0]*length
+
+        for i in range(length):
+            y_pred[i] = sum([self.weights[k]*self.classifier_list[k].gaussian_prob(X[i])
+                             for k in range(self.K)])
+
+        return y_pred
 
 
