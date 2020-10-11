eebbe92d8f13f4c9559f820f65324c36930325a8
laowang <1249402434@qq.com>
2020-10-11 16:15:45 +0800

    finished

--- model/SVM.py
+++ model/SVM.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.svm import SVC
 
 
 class SVC:
@@ -7,6 +6,7 @@ class SVC:
         self.kernel = kernel
         self.C = c
         self.b = 0
+        self.y = None
         self.alpha_list = None
         self.predict_prob = None
         if self.kernel == 'poly':
@@ -25,7 +25,7 @@ class SVC:
         elif self.kernel == 'linear':
             return x1.T.dot(x2)
 
-    def update(self, index1, index2, X, y, N):
+    def update(self, index1, index2, X, y, N, predict_prob):
         alpha1 = self.alpha_list[index1]
         alpha2 = self.alpha_list[index2]
         y1 = y[index1]
@@ -34,7 +34,7 @@ class SVC:
         x1 = X[index1]
         x2 = X[index2]
 
-        if y1!=y2:
+        if y1 != y2:
             low_limit = max(0, alpha2 - alpha1)
             high_limit = min(self.C, self.C+alpha2-alpha1)
 
@@ -42,13 +42,13 @@ class SVC:
             low_limit = max(0, alpha1+alpha2-self.C)
             high_limit = min(self.C, alpha2+alpha1)
 
-        K11 = self.calc_kernel_value(x1,x1)
-        K22 = self.calc_kernel_value(x2,x2)
-        K12 = self.calc_kernel_value(x1,x2)
+        K11 = self.calc_kernel_value(x1, x1)
+        K22 = self.calc_kernel_value(x2, x2)
+        K12 = self.calc_kernel_value(x1, x2)
 
         eta = K11+K22-2*K12
-        E1 = self.predict_prob[index1] - y1
-        E2 = self.predict_prob[index2] - y2
+        E1 = predict_prob[index1] - y1
+        E2 = predict_prob[index2] - y2
         alpha2_new = alpha2 + (y2*(E1 - E2)) / eta
 
         if alpha2_new > high_limit: alpha2_new = high_limit
@@ -56,24 +56,54 @@ class SVC:
         alpha1_new = alpha1 + y1*y2*(alpha2 - alpha2_new)
         self.alpha_list[index2] = alpha2_new
         self.alpha_list[index1] = alpha1_new
-
-        tmp_list = [self.alpha_list[index]*y[index]*self.calc_kernel_value(X[index],x1) for index in range(N) if index!=index1 and index!=index2]
+        """
+        除alpha1和alpha2之外的alpha_i*yi*K(i,1)
+        """
+        tmp_list = [self.alpha_list[index]*y[index]*self.calc_kernel_value(X[index],x1)
+                    for index in range(N) if index!=index1 and index!=index2]
 
         b1 = y1 - sum(tmp_list) - alpha1_new*y1*K11 - alpha2_new*y2*K12
         b2 = -E2-y1*K12*(alpha1_new - alpha1) -y2*K22(alpha2_new-alpha2)+self.b
         self.b = (b1 + b2) / 2
 
-    def fit(self, X, y):
+    def fit(self, X, y, epochs):
         N = X.shape[0]
-        self.alpha_list = [0] * N
+        self.y = y
+        self.alpha_list = [1/N] * N
 
-        for i in range(0, N-1, 2):
-            self.update(i, i+1, X, y, N)
+        predict_prob=self.predict(X)
+        """
+        每次选取两个alpha值去训练
+        """
+        for e in range(epochs):
+            for i in range(0, N-1, 2):
+                self.update(i, i+1, X, y, N, predict_prob)
 
     def predict(self, X):
+        if type(X) == list:
+            X = np.array(X)
+
         N = X.shape[0]
+        predict_prob = [0]*N
         for i in range(N):
-            self.predict_prob[i] = sum([self.alpha_list[i]*])
+            predict_prob[i] = sum([self.alpha_list[j]*self.y[j]*self.calc_kernel_value(X[i], X[j])+self.b
+                                   for j in range(N)])
+
+        return predict_prob
+
+    def predict_label(self, X):
+        if type(X) == list:
+            X = np.array(X)
+
+        predict_prob = self.predict(X)
+        label = [0]*len(predict_prob)
+        for i in range(len(predict_prob)):
+            if predict_prob[i] >= 0:
+                label[i] = 1
+            else:
+                label[i] = -1
+
+        return label
 
 
 
--- model/gaussian_blend_model.py
+++ model/gaussian_blend_model.py
@@ -4,6 +4,10 @@ import math
 
 class gaussian_model:
     def __init__(self, mean, cov_matrix):
+        """
+        :param mean: 这个高斯类的均值
+        :param cov_matrix: 协方差矩阵
+        """
         self.mean = mean
         self.cov_matrix = cov_matrix
         self.dim = len(mean)
@@ -22,6 +26,10 @@ class gaussian_model:
 
 class gaussian_EM:
     def __init__(self, K=3):
+        """
+
+        :param K: 高斯模型的个数
+        """
         self.K = K
         self.weights = [1 / K] * K
 
@@ -29,13 +37,22 @@ class gaussian_EM:
         if type(X) == list:
             X = np.array(X)
         N, dim = X.shape
+        """
+        K个高斯模型的均值矩阵和协方差矩阵的矩阵
+        """
         mean_arr = np.random.randn(self.K, dim)
         cov_matrix_arr = [np.diag(dim * [1]) for _ in range(self.K)]
 
         self.classifier_list = [gaussian_model(mean_arr[k], cov_matrix_arr[k]) for k in range(self.K)]
+        '''
+        存放gamma_nk值的矩阵
+        '''
         res_arr = []
         for e in range(epochs):
             for i in range(N):
+                '''
+                一个样本对每一个高斯模型的gamma_nk值
+                '''
                 gamma_nk_arr = []
                 total = 0
                 for k in range(self.K):
@@ -46,9 +63,15 @@ class gaussian_EM:
                 gamma_nk_arr = [gamma_nk_arr[k] / total for k in range(self.K)]
                 res_arr.append(gamma_nk_arr)
 
+            '''
+            Nk值列表，总的样本里面由多少个属于第k个高斯模型
+            '''
             Nk_list = [sum(list(zip(*res_arr))[k]) for k in range(self.K)]
             mean_new = []
             cov_matrix_arr_new = []
+            '''
+            计算更新后的均值和协方差矩阵
+            '''
             for k in range(self.K):
                 Nk = Nk_list[k]
                 self.weights[k] = Nk / N
@@ -61,7 +84,7 @@ class gaussian_EM:
                                                for i in range(N)])/Nk)
 
             for k in range(self.K):
-                self.classifier_list[k].update(mean_new, cov_matrix_arr_new)
+                self.classifier_list[k].update(mean_new[k], cov_matrix_arr_new[k])
 
     def predict(self, X):
         if type(X)==list:
